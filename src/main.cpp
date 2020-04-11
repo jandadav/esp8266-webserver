a4a63777b1a82871d4c86d9ebf734244521d4194
jandadav <janda.david@gmail.com>
2020-04-11 16:48:32 +0200

    mDNS working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include "WifiInit.h"
-
+#include <ESP8266mDNS.h>
 
 WifiAgent wifiHelper;
 
@@ -8,10 +8,20 @@ void setup(void) {
   Serial.begin(115200);
   Serial.println("Serial started");
   wifiHelper.start();
- 
+
+  if (!MDNS.begin("mcu")) {
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+
+  MDNS.addService("http", "tcp", 80);
 }
 
 void loop(void) {
 
+  MDNS.update();
 
 }
